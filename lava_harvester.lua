83690e0ac02f87a2dba272c8743101ef3bb16909
jamesL103 <jameskl7337@gmail.com>
2026-06-19 13:59:55 -0400

    Write out lava_harvester

--- lava_harvester.lua
+++ lava_harvester.lua
@@ -27,10 +27,22 @@ local function transferToInventory(buffer, dest)
     transferAllItems(buffer, dest)
 end
 
+local function return_to_start()
+    local front_block = turtle.inspect()
+    local up_block = turtle.inspectUp()
+    turtle.turnRight()
+    while front_block.name == "minecraft:chest" and up_block.name == "minecraft:chest" do
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnLeft()
+    end
+    turtle.turnLeft()
+end
+
 while true do
     -- wrap chest as peripheral
     local depot = peripheral.wrap("front") -- fuel depot chest
-    local buffer = peripheral.wrap("back") -- item transferring buffer chest
+    local buffer = peripheral.wrap("up") -- item transferring buffer chest
 
     --check own fuel level
     if turtle.getFuelLevel() < MIN_FUEL then
@@ -60,10 +72,44 @@ while true do
     -- check depot for empty buckets
     for slot, item in pairs(depot.list()) do
         if item ~= nil and item.name == "minecraft:bucket" then
-            depot.pushItems(buffer.getName(), slot) -- move only one lava bucket for now
+            depot.pushItems(buffer.getName(), slot)
         end
     end
+    turtle.suckUp()
 
+    -- get number of empty buckets
+    local empty_buckets = 0
+    for slot = 1, 16 do
+        if turtle.getItemDetail(slot).name == "minecraft:bucket" then
+            empty_buckets = empty_buckets + 1
+        end
+    end
+    if empty_buckets > 0 then
+        turtle.turnLeft()
+    end
     -- refuel empty buckets
+    for i = 1, cauldrons do
+        -- check if empty buckets left in inventory
+        if empty_buckets <= 0 then
+            break
+        end
+        turtle.forward()
+        turtle.turnLeft()
+        turtle.place()
+        turtle.turnRight()
+        empty_buckets = empty_buckets - 1
+        if empty_buckets <= 0 then
+            break
+        end
+        turtle.turnRight()
+        turtle.place()
+        turtle.turnLeft()
+        empty_buckets = empty_buckets - 1
+    end
+    return_to_start()
 
+    for slot = 1, 16 do
+        turtle.select(slot)
+        turtle.drop()
+    end
 end

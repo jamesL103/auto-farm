86e9c8f29c935f3e5b008b1711a846fb597e7b6a
jamesL103 <jameskl7337@gmail.com>
2026-06-21 15:33:31 -0400

    add refueling behavior

--- carrot_farm.lua
+++ carrot_farm.lua
@@ -11,7 +11,24 @@ else
     assert(width ~= nil and height ~= nil)
 end
 
-local MIN_FUEL = 400 -- TODO: create function relative to width and height
+local MIN_FUEL = 1000 -- TODO: create function relative to width and height
+
+local function refuel()
+    if chest == nil then
+        return
+    end
+
+    for _ in 1, 4 do
+        turtle.suckUp()
+    end 
+
+    for slot in 1, 4 do
+        turtle.select(slot)
+        turtle.refuel()
+        turtle.dropUp()
+    end
+
+end
 
 -- main farming loop
 while true do
@@ -28,7 +45,7 @@ while true do
 
     -- check for fuel count
     if turtle.getFuelLevel() < MIN_FUEL then
-        
+        refuel()
     end
 
     turtle.turnRight()

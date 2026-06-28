3aeb31a4def7c4fd0ce74871fff19cb46be3fdd3
jamesL103 <jameskl7337@gmail.com>
2026-06-27 23:05:08 -0400

    Add JSON config for storage

--- store.lua
+++ store.lua
@@ -1,11 +1,14 @@
 
 -- move all carrots from plot storage to central storage
-local function storeCarrots()
-    local farms = peripheral.wrap("top")
-    local central = peripheral.wrap("right")
+local function storeCarrots(carrotCentral)
+    local central = peripheral.wrap(carrotCentral)
     local chests = { peripheral.find("minecraft:chest")}
 
     for i, chest in pairs(chests) do
+        if chest.name == carrotCentral then
+            goto skip_central
+        end
+
         for slot, item in pairs(chest.list()) do
             if item == nil then
                 goto continue
@@ -13,15 +16,21 @@ local function storeCarrots()
             central.pullItems(peripheral.getName(chest), slot)
             ::continue::
         end
+        ::skip_central::
     end
     print("Moved carrots to central storage")
 end
 
 if shell and shell.getRunningProgram() == 'store.lua' then
+    -- Get the JSON config file
+    local file = io.open('storage.json')
+    local config = textutils.unserializeJSON(file.read("a"))
+
     local cmd = arg[1]
     if cmd == "carrot" then
-        storeCarrots()
+        storeCarrots(config.carrot_central)
     else 
         error("Invalid command")
     end
+    io.close(file)
 end
